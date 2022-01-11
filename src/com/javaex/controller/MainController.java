9173ad7566984b8be2307ecfcd75c34ac30d64ba
한상선 <hs94122@naver.com>
2022-01-11 13:54:33 +0900

    2022-01-11 13:54

---
+++ src/com/javaex/controller/MainController.java
@@ -0,0 +1,30 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javaex.util.WebUtil;
+
+@WebServlet("/main")
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("/main");
+		
+		//포워드 : 포워드는 파일위치를 지정해준다 <-> 리다이렉트는 url을 입력해 준다
+		WebUtil.forward(request, response, "/WEB-INF/views/main/index.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
